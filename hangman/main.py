7e926216935c56e5adc94caa42fe219d4703952f
Bret Walda <qafamunto@icloud.com>
2024-08-08 10:48:01 +0000

    create a file called utils.py and used it to store new functions list_selector and play_game for clarity of my code

--- hangman/main.py
+++ hangman/main.py
@@ -3,11 +3,19 @@ from hangman.ascii_art import logo
 
 #print the logo
 logo()
+play = True
+while play is True:
+    ##Menu and choice of the theme
+    theme = input("Quel thème préfères-tu?\nType fruits, musique, animaux\n")
 
-##Menu and choice of the theme
-theme = input("What theme do you prefer?\nType fruits, musique, animaux\n")
+    #choice of the list of word according to the theme chosen
+    chosen_list = list_selector(theme)
 
-#choice of the list of word according to the theme chosen
-chosen_list = list_selector(theme)
+    play_game(chosen_list)
 
-play_game(chosen_list)
+    play_again = input("Tu veux jouer encore?\nTape 'oui' ou 'non'")
+
+    if play_again == 'non':
+        play = False
+    else:
+        print("C'est parti pour un autre tour")
